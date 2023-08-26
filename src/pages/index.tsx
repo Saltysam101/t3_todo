5828c6698b281088c21860b8fb7861715514e620
Sam Zudell <s.zudell98@gmail.com>
2023-08-26 16:16:25 -0400

    removed anchor tag for index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
-import { SignIn, useUser } from "@clerk/nextjs";
+import { SignIn} from "@clerk/nextjs";
 
 export default function SignInPage() {
   
-const {isLoaded, isSignedIn} = useUser();
 
 
   return (
     <>
-    {!isLoaded || !isSignedIn ? 
+     
     <div>
       <h2>Hello There! Please sign in with your Github account.</h2>
       <SignIn />
-    </div> :
-    <div>You are already signed in. Please head on over to the <a href="/todo">todo page</a></div>
-    }
+    </div>
     
         
     </>
