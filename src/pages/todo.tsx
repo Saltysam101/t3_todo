6dc2455347ebf06755e4a88f0b82fe7e92a72800
Sam Zudell <s.zudell98@gmail.com>
2023-08-27 15:57:49 -0400

    made todo model for prisma

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -4,8 +4,17 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Todo() {
   return (
-    <div>
+    <>
+      <nav>
         <UserButton afterSignOutUrl="/" />
-    </div>
+      </nav>
+        <h2>Your Todo List</h2>
+      <form>
+        <input placeholder="Add Your Todo Here" type="text" />
+      </form>
+      <main>
+        <div>A todo item</div>
+      </main>
+    </>
   )
 }
