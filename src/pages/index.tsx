22a738578027f4bf340ae7c99784ffda9888fb93
Sam Zudell <s.zudell98@gmail.com>
2023-12-05 18:06:11 -0500

    centered signin button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,12 @@ export default function SignInPage() {
 const {user, isLoaded, isSignedIn} = useUser();
 
   return (
-    <>
+    <div className="h-full flex justify-center items-center">
      { !user || !isLoaded || !isSignedIn ? 
       <SignIn afterSignInUrl="/todo"/>
       : 
       <div>How are you still here?</div>
        }
-    </>
+    </div>
   );
 }
